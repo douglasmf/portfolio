1c82d60566e0e60a761ab59c122064fed03d2d13
Douglas Monteiro <douglas.m.f.1995@gmail.com>
2025-08-03 18:42:27 -0300

    Atualizando curriculo e sobre

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     <S.AboutContainer>
         <TitleSection title="Sobre Mim"/>
         <S.AboutText>
-          <p>Tenho muito interesse em aprender novas tecnologias e, no momento, venho me dedicando a aprender desenvolvimento front-end de forma autodidata, por meio de vídeos, artigos e documentações. Dessa forma, consigo focar meu aprendizado em tecnologias de maior interesse ou tendência, além de poder estudar no meu próprio ritmo.
+          <p>Olá, me chamo Douglas Monteiro, sou natural de Gravataí-RS e estou cursando Análise e Desenvolvimento de Sistemas pela Uninter. Tenho como objetivo me tornar um desenvolvedor fullstack, expandindo meus conhecimentos tanto no front quanto no back-end. No momento, estou focado em front-end, buscando criar interfaces modernas e funcionais, além de aprender novas tecnologias que possam contribuir nos projetos que participo.
           </p>
           <p>Minhas principais habilidades envolvem HTML, CSS, JavaScript e React. Estou sempre aberto a explorar novas ferramentas e frameworks que possam agregar valor aos projetos. Atualmente, estou em busca da minha primeira oportunidade para aplicar e expandir meus conhecimentos.</p>
         </S.AboutText>
